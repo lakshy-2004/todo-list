a3ec285a761fa133c3f8b8d22790c25c0c97b773
Lakshya Sharma <116435569+lakshy-2004@users.noreply.github.com>
2026-05-16 00:37:12 +0530

    Update Footer.jsx
    Edit: footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className='container-fluid p-3 d-flex justify-content-center align-items-center text-white footer'>
         <h4>todo</h4>
-        &nbsp;<p className='m-0'>&copy;Lakshya Sharma</p>
+        &nbsp;<p className='m-0'>&copy;</p>
     </div>
   )
 }
